991bb135fe007bfd041885e41447a53b06be8a04
Charbel <charbel.dawoud@gmail.com>
2024-12-08 09:50:50 +0100

    JS <> Finalizing Scoring System

--- game.js
+++ game.js
@@ -10,24 +10,24 @@ let status_display;
 
 document.addEventListener("DOMContentLoaded", loadPage);
 
-function displayScore(){
-    status_display.innerHTML = "Your Score is: " + score;
+function displayScore(message){
+    if(message != "")
+        status_display.innerHTML = message + "<br/>" + "Your Score is: " + score;
 }
 
 function gameOver(){
     if(is_game_running){
         for(let i = 0; i < boundaries.length; i++)
             boundaries[i].style.backgroundColor = "rgb(243, 159, 159)"; 
-        if(score > 0){
+        if(score > 0)
             score = score - 1;
-            displayScore();
-        }
+        displayScore("Game Over!");
         is_game_running = false;
     }
 }
 
 function startGame(){
-    displayScore();
+    displayScore("");
     is_game_running = true;
     for(let i = 0; i < boundaries.length; i++)
         boundaries[i].style.backgroundColor = "#eeeeee"; 
@@ -35,7 +35,11 @@ function startGame(){
 
 function endGame(){
     if(is_game_running){
-        alert("Mabrouk!");
+        for(let i = 0; i < boundaries.length; i++)
+            boundaries[i].style.backgroundColor = "rgb(113 225 141)"; 
+        score = score + 5;
+        displayScore("You Won!");
+        is_game_running = false;
     }
 }
 
